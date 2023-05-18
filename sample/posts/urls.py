6e6cd092864c2cfe7b2d52a4692822b3526edfb0
pranav e8 <pranave8@pranavs-Mac-mini.local>
2023-05-18 15:53:41 +0530

    completednew

--- sample/posts/urls.py
+++ sample/posts/urls.py
@@ -1,31 +1,23 @@
 from django.urls import path
-from .views import (
+
+from posts.views import (
     posts_lists_and_create,
     load_post_data_view,
     like_unlike_post,
-    load_post_data_view,
     post_detail,
     post_detail_data_view,
     update_post,
     delete_post,
-     
-    
-    
-  
+
 )
 app_name = 'posts'
- 
 urlpatterns = [
-    
-    path('',posts_lists_and_create,name='main-board'),
-    path('data/<int:num_posts>/',load_post_data_view,name='posts-data'),
-    path('like-unlike/',like_unlike_post,name='like-unlike'),
+    path('', posts_lists_and_create, name='main-board'),
+    path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
+    path('like-unlike/', like_unlike_post, name='like-unlike'),
     # path('<pk>/',post_detail,name='posts-detail'),
-    path('<pk>/data/',post_detail_data_view,name='post-detail-data'),
-    
-     path('<pk>/update/',update_post,name='post-update'),
-     path('<pk>/delete/',delete_post,name='post-delete'),
-     
-    path('<pk>/',post_detail,name='posts-detail'),
-
-] 
+    path('<pk>/data/', post_detail_data_view, name='post-detail-data'),
+    path('<pk>/update/', update_post, name='post-update'),
+    path('<pk>/delete/', delete_post, name='post-delete'),
+    path('<pk>/', post_detail, name='posts-detail'),
+]
--- sample/posts/views.py
+++ sample/posts/views.py
@@ -4,10 +4,9 @@ from django.http import JsonResponse
 from .forms import PostForm
 from profiles.models import Profile
 
+
 def posts_lists_and_create(request):
     form = PostForm(request.POST or None)
-    # qs = Post.objects.all()
-    
     if request.method == 'POST':
         if form.is_valid():
             author = Profile.objects.get(user=request.user)
@@ -15,50 +14,46 @@ def posts_lists_and_create(request):
             instance.author = author
             instance.save()
             return JsonResponse({
-                'title' : instance.title,
-                'body'  : instance.body,
-                'author' : instance.author.user.username,
-                'id' : instance.id,
-            })
-            
+                'title': instance.title,
+                'body': instance.body,
+                'author': instance.author.user.username,
+                'id': instance.id,
+                })
     context = {
-        'form' : form,
-    }        
-    return render(request,'posts/main.html', context)
+        'form': form,
+        }
+    return render(request, 'posts/main.html', context)
 
 
-def post_detail(request,pk):
+def post_detail(request, pk):
     obj = Post.objects.get(pk=pk)
     form = PostForm()
-    
     context = {
-        'obj' : obj,
-        'form' : form
+        'obj': obj,
+        'form': form
     }
 
-    return  render(request, 'posts/detail.html',context)
+    return render(request, 'posts/detail.html', context)
 
-def load_post_data_view(request,num_posts):
-        visible = 3
-        upper = num_posts
-        lower = upper - visible
-        size = Post.objects.all().count()
-        
-        
-        qs = Post.objects.all()
-        data = []
-        for obj in qs:
-            item = {
+
+def load_post_data_view(request, num_posts):
+    visible = 3
+    upper = num_posts
+    lower = upper - visible
+    size = Post.objects.all().count()
+    qs = Post.objects.all()
+    data = []
+    for obj in qs:
+        item = {
                 'id': obj.id,
                 'title': obj.title,
                 'body': obj.body,
-                'liked' : True if request.user in obj.liked.all() else False,
-                'count' : obj.like_count,
-                'author' : obj.author.user.username
+                'liked': True if request.user in obj.liked.all() else False,
+                'count': obj.like_count,
+                'author': obj.author.user.username
             }
-            data.append(item)
-        return JsonResponse({'data':data[lower:upper], 'size' : size})
-
+        data.append(item)
+    return JsonResponse({'data': data[lower:upper], 'size': size})
 
 
 def post_detail_data_view(request, pk):
@@ -67,11 +62,11 @@ def post_detail_data_view(request, pk):
         'id': obj.id,
         'title': obj.title,
         'body': obj.body,
-        'author' : obj.author.user.username,
-        'logged_in' : request.user.username,
-        
-    }
-    return JsonResponse({'data' : data})
+        'author': obj.author.user.username,
+        'logged_in': request.user.username,
+        }
+    return JsonResponse({'data': data})
+
 
 def like_unlike_post(request):
     if request.method == 'POST':
@@ -81,13 +76,12 @@ def like_unlike_post(request):
             liked = False
             obj.liked.remove(request.user)
         else:
-            liked = True  
+            liked = True
             obj.liked.add(request.user)
-        return JsonResponse({'liked': liked, 'count' : obj.like_count})
-    
+        return JsonResponse({'liked': liked, 'count': obj.like_count})
 
 
-def update_post(request,pk):
+def update_post(request, pk):
     obj = Post.objects.get(pk=pk)
     if request.method == 'POST':
         new_title = request.POST.get('title')
@@ -96,13 +90,12 @@ def update_post(request,pk):
         obj.body = new_body
         obj.save()
         return JsonResponse({
-            'title' : new_title,
-            'body' : new_body,
+            'title': new_title,
+            'body': new_body,
         })
 
 
-
-def delete_post(request,pk):
+def delete_post(request, pk):
     obj = Post.objects.get(pk=pk)
     if request.method == 'POST':
         obj.delete()
